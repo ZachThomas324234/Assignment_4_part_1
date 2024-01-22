fdf8cc1b6fc90bc0d4b19b84349eed75a45d3a80
ZachThomas324234 <539123@chrome.escg.ac.uk>
2024-01-22 03:52:36 +0000

    script and enemies
    heal
    health
    enemy models

---
+++ Assets/Scripts/Heal.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour
+{
+
+    public int PlayerMaxHealth = 100;
+    public int PlayerHealth = 100;
+    public int HealAmount = 30;
+    public int MeatAmountMax = 3;
+    public int MeatAmount = 3;
+
+    public int PlayerDamage = 25;
+
+    public int EnemyMaxHealth = 25;
+    public int EnemyDamage = 15;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                PlayerHealth += HealAmount;
+                MeatAmount -= 1;
+            }
+        
+    if (MeatAmount < 1)
+        {
+        HealAmount = 0;
+        }
+
+        if (PlayerHealth >= PlayerMaxHealth)
+        {
+        PlayerHealth = PlayerMaxHealth;
+        }
+    }
+}
